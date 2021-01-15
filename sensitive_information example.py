6eeee0b19af4feb1ce8d57143d71e999fb5b290f
frozenbits <47202084+frozenbits@users.noreply.github.com>
2021-01-16 00:56:09 +0700

    added sensitive_information example

---
+++ sensitive_information example.py
@@ -0,0 +1,19 @@
+# Flask
+tokens = {
+    "some_random_token": "the_id"
+}
+
+# Yeelight
+lamp_ip = "1.2.3.4"
+
+
+# Led_Strip
+red_pin = 1
+green_pin = 2
+blue_pin = 3
+
+# Button
+button_pin = 4
+
+# DHT11 / Temperature
+temperature_pin = 5
